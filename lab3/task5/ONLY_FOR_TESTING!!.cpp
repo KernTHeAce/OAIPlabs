048f7536ad9a90dbb3a9a02bcb51cd5b4cdee3ac
Kern_THe_Ace <64127128+KernTHeAce@users.noreply.github.com>
2020-06-23 21:27:39 +0300

    Create ONLY_FOR_TESTING!!.cpp

---
+++ lab3/task5/ONLY_FOR_TESTING!!.cpp
@@ -0,0 +1,49 @@
+﻿#include<iostream>
+#include<fstream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+
+	ofstream out;
+	ifstream in;
+
+	out.open("D:\\rq.txt");
+	in.open("D:\\q.txt");
+
+
+	if (!in.is_open() && !out.is_open())
+	{
+		cout << "Error" << endl;
+	}
+	else
+	{
+		cout << "file is open" << endl;
+
+		string str;
+		string line;
+		while (getline(in, str))
+		{
+			line += str;
+		}
+		string lin;
+		for (int i = 0; i < 255; i++)
+		{
+			for (int j = 0; j < line.length() - 1; j++)
+			{
+				if (i == (int)line[j])
+				{
+					lin += line[j];
+					break;
+				}
+			}
+		}
+
+		out << lin;
+
+		system("pause");
+		return 0;
+	}
+}
